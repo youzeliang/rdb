24e80bcf8d4b480442b3192d5076deb807868b32
youze <youzel@126.com>
2023-05-07 23:47:19 +0800

    add data_file

--- data/log_record.go
+++ data/log_record.go
@@ -4,3 +4,18 @@ type LogRecordPos struct {
 	Fid    uint32 // file id
 	Offset int64  // Offset refers to the position where data is stored in a data file.
 }
+
+type LogRecordType = byte
+
+const (
+	LogRecordNormal LogRecordType = iota
+	LogRecordDeleted
+)
+
+// LogRecord 写入到数据文件的记录
+// 之所以叫日志，是因为数据文件中的数据是追加写入的，类似日志的格式
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType
+}
--- index/btree.go
+++ index/btree.go
@@ -2,7 +2,7 @@ package index
 
 import (
 	"github.com/google/btree"
-	"github.com/youzeliang/rdb/data"
+	"rdb/data"
 	"sync"
 )
 
