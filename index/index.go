2873dbe320fe2c187fcaf6b18cd030fdd52c1a00
youze <youzel@126.com>
2023-05-08 23:57:31 +0800

    add test

--- index/index.go
+++ index/index.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"bytes"
 	"github.com/google/btree"
 	"rdb/data"
 )
@@ -10,7 +11,17 @@ type Item struct {
 	pos *data.LogRecordPos
 }
 
-func (i Item) Less(than btree.Item) bool {
-	//TODO implement me
-	panic("implement me")
+func (i *Item) Less(than btree.Item) bool {
+	return bytes.Compare(i.key, than.(*Item).key) == -1
+}
+
+type Indexer interface {
+	// Put store the position information of data corresponding to the key in the index.
+	Put(key []byte, pos *data.LogRecordPos) bool
+
+	// Get the position information of data corresponding to the key in the index.
+	Get(key []byte) *data.LogRecordPos
+
+	// Delete the position information of data corresponding to the key in the index.
+	Delete(key []byte) bool
 }
